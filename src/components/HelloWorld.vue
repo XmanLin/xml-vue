d8cdee118851c4daf3fa2f6a49f98e0fe255c115
XmanLin <1028343150@qq.com>
2020-07-02 23:55:58 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 @click="getMsg">哈哈哈</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '欢迎你~'
+    }
+  },
+  methods: {
+    getMsg(){
+      console.log(this.msg)
+    }
+  },
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
